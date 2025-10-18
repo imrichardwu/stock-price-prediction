07f65bff3ea96b0298821244cbfd7a06d55992d5
Richard Wu <richard9@ualberta.ca>
2025-10-17 18:17:31 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -24,11 +24,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "http://localhost:3001", 
-        "http://localhost:3002", 
-        "http://localhost:3003", 
-        "http://localhost:3004",
-        "http://localhost:3005"
     ],
     allow_credentials=True,
     allow_methods=["*"],
